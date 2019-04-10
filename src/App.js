89fa68b6e6e134094540c1b17d786066c5f36e56
olivier Tristan <tristanolivier.work@gmail.com>
2019-04-10 13:55:11 +0200

    component dropdown apparence

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import lien from './components/dopdown'
+import Dropdown from './Components/Dropdown/Dropdown'
+
+// import DropdownV2 from './Components/Dropdown/DropdownV2'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-         
-       
-         
-        </header>
-      </div>
+     <div>
+       <Dropdown/>
       
+      
+        {/* <DropdownV2/> */}
+      </div>
         
-          <FORM NAME="Choix">
-          <SELECT NAME="Liste" onChange="Lien()">
-          <OPTION VALUE="">Choisir une option
-          <OPTION VALUE="../../copains.html">Les copains
-          <OPTION VALUE="../../plongee/index.html">La plongée
-          <OPTION VALUE="http://www pt.google.com">Recherche
-          </SELECT>
-          </FORM>
-          
       
     );
   }
---
+++ src/Components/Dropdown/Dropdown.js
@@ -0,0 +1,54 @@
+
+//  import React, { Component } from 'react'
+ import React from 'react';
+ import './Dropdown.css';
+ import imgHistory from './images/icon-history.svg'
+
+
+ class HistoryItem extends React.Component {
+   render(){
+     return (
+     <div>
+        <div className="history-item">
+        <i class="material-icons">history</i>
+          <p>{this.props.title}</p>
+        </div>
+      </div>
+      )
+   }
+ }
+ class ResultItem extends React.Component {
+  render(){
+    return (
+    <div>
+       <div className="resultItem">
+        <p>{this.props.title}</p>
+       </div>
+     </div>
+     )
+  }
+}
+
+class Dropdown extends React.Component {
+    render() {
+      return (
+      <div className="dropdown">
+        <ResultItem title = "Machete"/>
+        <HistoryItem title = "Star wars"/>
+        <HistoryItem title = "Star wars 2"/>
+        <HistoryItem title = "Star wars 3"/>
+        </div>
+      
+      );
+    }
+  }
+
+  export default Dropdown 
+
+
+
+
+
+
+
+
