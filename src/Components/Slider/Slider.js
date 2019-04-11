fb3cddf1edb68d667875b49bb6a31b2867197257
Alexandre Coutant <alexcoutant3@gmail.com>
2019-04-11 16:12:12 +0200

    nouveau carrouseel

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,149 +1,120 @@
-import React from 'react';
-import './Slider.css';
-import $ from "jquery";
+ import React from 'react';
+ import './Slider.css';
+ import $ from "jquery";
 
-let scaling = 1.50;
-//count
-let currentSliderCount = 0;
-let videoCount = $(".slider-container").children().length;
-let showCount = 4;
-let sliderCount = videoCount / showCount;
-let controlsWidth = 40;
-let scollWidth = 0;
+// let scaling = 1.50;
+// //count
+// let currentSliderCount = 0;
+// let videoCount = $(".slider-container").children().length;
+// let showCount = 4;
+// let sliderCount = videoCount / showCount;
+// let controlsWidth = 40;
+// let scollWidth = 0;
     
-
-$(document).ready(function(){
-    //$('.slider-container .slide:nth-last-child(-n+4)').prependTo('.slider-container');
-    init();
-    
-});
-$( window ).resize(function() {
-    init();
-});
-function init(){
-    // elements
-  let win = $(window);
-    let sliderFrame = $(".slider-frame");
-    let sliderContainer = $(".slider-container");
-    let slide = $(".slide");
-    
-    //counts
-    let scollWidth = 0;
  
     
-    //sizes
-    let windowWidth = win.width();
-    let frameWidth = win.width() - 80;
-     if(windowWidth >= 0 && windowWidth <= 414){
-       showCount = 2;
-   }else if(windowWidth >= 414 &&  windowWidth <= 768){
-       showCount = 3;
-   }else{
-       showCount = 4;
-   }
-    let videoWidth = ((windowWidth - controlsWidth * 2) / showCount );
-    let videoHeight = Math.round(videoWidth / (16/9));
-    
-    let videoWidthDiff = (videoWidth * scaling) - videoWidth;
-    let videoHeightDiff = (videoHeight * scaling) - videoHeight;
-    
+
+// const Slider = () => { 
   
+//     return(
+//       <div class="slider-frame">
     
-    //set sizes
-    sliderFrame.width(windowWidth);
-    sliderFrame.height(videoHeight * scaling);
-    
-    
-    //sliderFrame.css("top", (videoHeightDiff / 2));
-    
-    sliderContainer.height(videoHeight * scaling);
-    sliderContainer.width((videoWidth * videoCount) + videoWidthDiff);
-    sliderContainer.css("top", (videoHeightDiff / 2));
-    sliderContainer.css("margin-left", (controlsWidth));
-    
-    slide.height(videoHeight);
-    slide.width(videoWidth);
-    
-    //hover effect
-    $(".slide").mouseover(function() {
-        $(this).css("width", videoWidth * scaling);
-        $(this).css("height", videoHeight * scaling);
-        $(this).css("top", -(videoHeightDiff / 2));
-        if($(".slide").index($(this)) == 0 || ($(".slide").index($(this))) % 4 == 0){
-          // do nothing
-        }
-        else if(($(".slide").index($(this)) + 1) % 4 == 0 && $(".slide").index($(this)) != 0){
-            $(this).parent().css("margin-left", -(videoWidthDiff - controlsWidth));
-        }
-        else{
-            $(this).parent().css("margin-left", - (videoWidthDiff / 2));
-        }
-    }).mouseout(function() {
-        $(this).css("width", videoWidth * 1);
-        $(this).css("height", videoHeight * 1);
-        $(this).css("top", 0);
-        $(this).parent().css("margin-left", controlsWidth);
-    });
-    
-    // controls
-    controls(frameWidth, scollWidth);
-}
-function controls(frameWidth, scollWidth){
-    var prev = $(".prev");
-    var next = $(".next");
-    
-    next.on("click", function(){
-        console.log(currentSliderCount);
-        console.log(sliderCount);
-        scollWidth = scollWidth + frameWidth;
-        $('.slider-container').animate({
-            left: - scollWidth
-        }, 300, function(){ 
-            if(currentSliderCount >= sliderCount-1){
-                $(".slider-container").css("left", 0);
-                currentSliderCount = 0;
-                scollWidth = 0;
-            }else{
-                currentSliderCount++;
-            }
-        });        
-    });
-    prev.on("click", function(){
-        scollWidth = scollWidth - frameWidth;
-        $('.slider-container').animate({
-            left: + scollWidth
-        }, 300, function(){ 
-            currentSliderCount--;
-        });
-        //$(".slider-container").css("left", scollWidth);
-    });
-};
+//     <div class="slider-container">
+//     {/* <div class="btn prev"></div>
+//     <div class="btn next"></div> */}
+//         <div class="slide"><img src="https://vignette.wikia.nocookie.net/harrypotter/images/f/f1/Affichefilm_HP1.jpg/revision/latest/scale-to-width-down/250?cb=20120819063135&path-prefix=fr" /></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/169796-collateral-affiche.jpg"/></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/FC1F26-million-dollar-baby-affiche.jpg"/></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/A0C659-hannibal-affiche.jpg"/></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
+//         <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
+//     </div>
+//   </div>
+//   )
  
-    
+// }
+
+
+
+//JS 
+
+// const slider = document.querySelector('.items');
+// let isDown = false;
+// let startX;
+// let scrollLeft;
+// //Definit une fonction slider qui prend items en paramètre et declare des variable.
+
+// slider.addEventListener('mousedown', (e) => {
+//   //prend slider et 
+//   isDown = true;
+//   slider.classList.add('active');
+//   startX = e.pageX - slider.offsetLeft;
+//   scrollLeft = slider.scrollLeft;
+// });
+// slider.addEventListener('mouseleave', () => {
+//   isDown = false;
+//   slider.classList.remove('active');
+// });
+// slider.addEventListener('mouseup', () => {
+//   isDown = false;
+//   slider.classList.remove('active');
+// });
+// slider.addEventListener('mousemove', (e) => {
+//   if(!isDown) return;
+//   e.preventDefault();
+//   const x = e.pageX - slider.offsetLeft;
+//   const walk = (x - startX) * 3; //scroll-fast
+//   slider.scrollLeft = scrollLeft - walk;
+//   console.log(walk);
+// });
+
+
+
+
+
+
+
 
 const Slider = () => { 
   
-    return(
-      <div class="slider-frame">
+   return(
+
+<div class="grid-container">
+  <header class="grid-item header">
+    <h2>🍽 CSS Grid Tem-plated 🍽</h2>
     
-    <div class="slider-container">
-    {/* <div class="btn prev"></div>
-    <div class="btn next"></div> */}
-        <div class="slide"><img src="https://vignette.wikia.nocookie.net/harrypotter/images/f/f1/Affichefilm_HP1.jpg/revision/latest/scale-to-width-down/250?cb=20120819063135&path-prefix=fr" /></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/169796-collateral-affiche.jpg"/></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/FC1F26-million-dollar-baby-affiche.jpg"/></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/A0C659-hannibal-affiche.jpg"/></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
-        <div class="slide"><img src="https://assets.e-cinema.com/UPLOADS/622134-zodiac-affiche.jpg"/></div>
-    </div>
+  </header>
+  <div class="grid-item title">
+    <h1>So you wanna side scroll, eh?</h1>
   </div>
-  )
- 
+  <main class="grid-item main">
+    <div class="items">
+      <div class="item item1"></div>
+      <div class="item item2"></div>
+      <div class="item item3"></div>
+      <div class="item item4"></div>
+      <div class="item item5"></div>
+      <div class="item item6"></div>
+      <div class="item item7"></div>
+      <div class="item item8"></div>
+      <div class="item item9"></div>
+      <div class="item item10"></div>
+    </div>
+    <p>🐰🥚Click & Drag <i>powered by Javascript</i></p>
+  </main>
+  <footer class="grid-item footer">
+    <h2>More Things</h2>
+    <a>All of the Things</a>
+    <a>Some of the Things</a>
+    <a>None of the Things</a>
+  </footer>
+</div>
+   )
 }
 
-export default Slider;
+ export default Slider;
