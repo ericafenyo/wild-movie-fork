cf4be8e8e1815283ec49779d9a0bafb72d814a86
olivier Tristan <tristanolivier.work@gmail.com>
2019-04-17 14:27:15 +0200

    favorite page without fav link and info link

---
+++ src/Components/FavoritePage/FavoritePage.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { search } from "../../data/ApiEndpoint";
+import { mapper } from "../../data/Mapper"
+import './FavoritePage.css'
+
+
+const FavIcon = ({ url, openDetail, adFavorite }) => {
+  return (
+
+    <div>
+
+      <img 
+      onClick={openDetail}
+      className="sizeImg img-fluid"
+        src={url}
+        alt="poster_path"
+      ></img>
+      <div className="icon-favorite" >
+      <i class="material-icons"onClick={adFavorite}> favorite </i>
+      </div>
+    </div>
+
+  );
+};
+
+class FavoritePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      isLoading: true,
+
+    }
+  };
+
+  componentDidMount() {
+    search('Lord', response => {
+      this.setState({ results: response })
+      this.setState({ isLoading: false })
+
+    });
+  }
+
+  openetail = () => {
+    console.log("test")
+  }
+  adFavorite = () => {
+    console.log("test")
+  }
+
+
+
+
+  render() {
+    return (
+      
+        <div className="row p-0 m-0">
+          {
+            this.state.results.map(item =>
+              (
+                < div className="col-6  favorite-item m-0 p-0">
+                  <FavIcon url={mapper.buildImageUrl(item.poster_path)} openDetail={this.openetail} adFavorite={this.adFavorite} />
+                </div>
+              )
+            )
+          }
+        
+      </div>
+    );
+  }
+};
+
+export default FavoritePage;
