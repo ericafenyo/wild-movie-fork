2f2d8e721982ed5e4f94c81856cd68bdcce8cbe5
Jennifer Boultareau <jennifer.boultareau@gmail.com>
2019-04-25 10:11:19 +0200

    petites modifs sliders avant merge

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -26,4 +26,19 @@ const Slider = ({data}) => {
   );
 }
 
+
+class Slider extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      
+    }
+  }
+}
+render () {
+  return (
+
+  )
+}
+
 export default Slider;
