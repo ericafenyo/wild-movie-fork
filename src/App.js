4a65c410574da09c9276e46ccd625aad58400421
Simon Philouze <contact@simonphilouze.fr>
2019-04-30 17:40:13 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, withRouter, Switch } from "react-router-dom";
+import { Route, withRouter, Switch } from "react-router";
 import HomeScreen from "./Components/HomeScreen/HomeScreen"
 import FavoritePage from './Components/FavoritePage/FavoritePage';
 import MovieInfo from './Components/MovieInfo/MovieInfo';
@@ -12,9 +12,9 @@ class App extends Component {
     return (
       <Switch>
         <Route path={process.env.PUBLIC_URL+'/'} exact component={HomeScreen} />
-        <Route exact path={process.env.PUBLIC_URL+'/movies'} component={SearchScreen} />
-        <Route exact path={process.env.PUBLIC_URL+'/favorites'} component={FavoritePage} />
-        <Route exact path={process.env.PUBLIC_URL+'/info'} component={MovieInfo} />
+        <Route path={process.env.PUBLIC_URL+'/movies'} component={SearchScreen} />
+        <Route path={process.env.PUBLIC_URL+'/favorites'} component={FavoritePage} />
+        <Route path={process.env.PUBLIC_URL+'/info'} component={MovieInfo} />
       </Switch>
     );
   }
