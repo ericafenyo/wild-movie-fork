124d15d1923e3caf4d65bbf6b7288d7e6f675f5a
Anais <anais.chevallier@gmail.com>
2019-04-30 15:33:56 +0200

    adding fav button on modal

--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, useState } from 'react';
 import { mapper } from "../../data/Mapper";
 import Casting from "../Casting/Casting";
 import StarRatings from "react-star-ratings";
@@ -6,6 +6,9 @@ import "./MovieDetails.css";
 import playButton from './icon-play.svg';
 
 const Detail = (props) => {
+  // Hook variable
+  const [favList, setFavList] = useState(props.clicked);
+
   return (
     <div className="movie-details vh-100  background-secondary">
       <div className="backdrop-wrapper">
@@ -32,12 +35,12 @@ const Detail = (props) => {
             <p className="info-color">{props.duration + " min | " + props.genre}</p>
             <p className="info-color">{props.director}</p>
             <p className="text-body- d-none d-md-block">{props.synopsis}</p>
-            <div className="" onClick= {props.manageMovie}>
-                      <i className="material-icons" >favorite</i>
-                      </div>
+            <div className="" onClick={() => { props.manageMovie(); setFavList(!favList) }}>
+              <i className={favList ? "material-icons favorite-clicked" : "material-icons favorite-null"} >favorite</i>
+            </div>
           </div>
         </div>
-      
+
       </div>
       <p className="body-text mx-3 d-md-none">{props.synopsis}</p>
       <Casting casts={props.cast} />
@@ -50,11 +53,9 @@ class MovieDetails extends Component {
     super(props);
     this.state = {
       movies: [],
-      heart:false,
-      
+      heart: false,
     }
   }
-    
 
   launchYoutube = (youtubeUrl) => {
     if (youtubeUrl) {
@@ -62,22 +63,21 @@ class MovieDetails extends Component {
     }
   }
 
-  manageMovie = (id) =>{
+  manageMovie = (id) => {
     if (JSON.parse(localStorage.getItem('favoris'))) {
-      if (JSON.parse(localStorage.getItem('favoris').includes(id))){
-         this.setState({heart: true})
+      if (JSON.parse(localStorage.getItem('favoris').includes(id))) {
+        this.setState({ heart: true })
 
         let favoris = JSON.parse(localStorage.getItem('favoris'));
         let index = favoris.indexOf(id);
         favoris.splice(index, 1);
         localStorage.setItem('favoris', JSON.stringify(favoris));
-    }else{
-      this.setState({heart: false})
-      localStorage.setItem('favoris', JSON.stringify([...JSON.parse(localStorage.getItem('favoris')), id]))
+      } else {
+        this.setState({ heart: false })
+        localStorage.setItem('favoris', JSON.stringify([...JSON.parse(localStorage.getItem('favoris')), id]))
+      }
     }
   }
-}
-  
 
   render() {
     return (
@@ -88,10 +88,10 @@ class MovieDetails extends Component {
           title={this.props.info.title}
           rating={this.props.info.vote_average / 2}
           duration={this.props.info.runtime}
-          genre={this.props.info.genres.length? this.props.info.genres[0].name : "N/A"}
-          manageMovie ={() =>this.manageMovie(this.props.info.id)}
+          genre={this.props.info.genres.length ? this.props.info.genres[0].name : "N/A"}
+          manageMovie={() => this.manageMovie(this.props.info.id)}
           synopsis={this.props.info.overview}
-          
+
           cast={this.props.info.credits.cast}
           launchYoutube={() => this.launchYoutube(mapper.parseYoutubeUrl(this.props.info.videos))}
         />
