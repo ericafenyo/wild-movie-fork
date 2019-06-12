3fd7889c92ff8fbced27202363dacc9cb985a6aa
ericafenyo <ericafenyo@yahoo.com>
2019-06-12 19:06:06 +0200

    add basic app test

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import App from './App';
+import { mount } from 'enzyme';
+
+describe('App', () => {
+    it('should successfully render App component', () => {
+        mount(<App />)
+    })
+    
+})
