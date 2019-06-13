eb9012f912b865cf9d755be4b9c652f272793ad3
ericafenyo <ericafenyo@yahoo.com>
2019-06-13 07:14:25 +0200

    update configuration

--- config-overrides.js
+++ config-overrides.js
@@ -3,8 +3,10 @@ const path = require('path');
 module.exports = (config, env) => {
   config.resolve = {
     alias: {
-      'components': path.resolve(__dirname, 'src/components'),
-    }
-  }
+      components: path.resolve(__dirname, 'src/components'),
+      mapper: path.resolve(__dirname, 'src/data/Mapper'),
+      'view-states': path.resolve(__dirname, 'src/components/view-states')
+    },
+  };
   return config;
-}
+};
